48fbe60888d3054eed326bdb879007bdf3fafe4c
TNT-A <andrejr.snellings@gmail.com>
2025-08-09 19:40:11 -0400

    final push

--- main/main.gd
+++ main/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var test_started : bool = false
+var can_play : bool = false
 var val : int = 0
 
 var sets : Dictionary = {
@@ -9,29 +10,68 @@ var sets : Dictionary = {
 }
 
 var resource : Resource
-var prompt_list : Array
-var answer_list : Array
+var test_list : Array
+
+#var prompt_list : Array
+#var answer_list : Array
 
 func _ready() -> void:
 	SignalBus.start_test.connect(start_test)
-	SignalBus.button_pressed.connect(check_win)
+	SignalBus.yes_clicked.connect(done)
+	SignalBus.no_clicked.connect(reroll)
+	SignalBus.reset_test.connect(reset)
 
+func reset():
+	var rand_hsv = randf_range(0.0, 1)
+	var saturation = randf() * 0.5 + 0.5
+	var value = randf() * 0.5 + 0.5
+	var new_color = Color.from_hsv(rand_hsv, saturation, value, 90)
+	box.color = new_color
+	$AnimationPlayer.play("RESET")
+	$Victory.reset()
+
+var box
 func start_test(type):
+	can_play = true
 	print(type)
 	if !test_started:
+		#test_started = true
 		resource = sets[type]
-		print(resource)
-		$RoundName.icon_img.texture = resource.icon_symbol
-		$RoundName.icon_name.text = resource.icon_text
-		prompt_list = resource.prompt_list
-		answer_list = resource.answer_list
+		print($RoundName)
+		#$RoundName/Icon.texture = resource.icon_symbol
+		$RoundName/CenterContainer/VBoxContainer/Name.text = resource.icon_text
+		box = $RoundName/CenterContainer/VBoxContainer/ColorRect
+		test_list = resource.test_list
+		#prompt_list = resource.prompt_list
+		#answer_list = resource.answer_list
 		print("Oh yea!")
 
+func done():
+	print("Got it right!!!!!")
+	can_play = false
+	$AnimationPlayer.play("fade_win")
+	get_tree().create_timer(.2)
+	if current_color != null:
+		$Victory/ColorRect.color = current_color
+	$Victory.fade_in()
+
+var current_color
+func reroll():
+	var rand_hsv = randf_range(0.0, 1)
+	var saturation = randf() * 0.5 + 0.5
+	var value = randf() * 0.5 + 0.5
+	var new_color = Color.from_hsv(rand_hsv, saturation, value, 90)
+	if can_play:
+		current_color = new_color
+		box.color = new_color
+		print("rerolling...", rand_hsv)
+
 func check_win(color_guess):
-	if answer_list[val] == color_guess:
-		print("true...", color_guess)
-	else:
-		print("WRONG L BOZO")
+	pass
+	#if answer_list[val] == color_guess:
+		#print("true...", color_guess)
+	#else:
+		#print("WRONG L BOZO")
 
 func set_image():
 	pass
